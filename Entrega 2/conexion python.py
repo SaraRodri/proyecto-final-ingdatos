a2b5eac60ad8cf4e5d00cd9543bf3d74429879e0
valMontl <117601204+valMontl@users.noreply.github.com>
2022-11-10 00:50:14 -0500

    Update conexion python.py

--- Entrega 2/conexion python.py
+++ Entrega 2/conexion python.py
@@ -29,7 +29,6 @@ conexion.close()
 
 
 ############### Base de datos de la tabla -> usuario ########
-
 cursor=conexion.cursor()
 sql ='SELECT * FROM usuario'
 cursor.execute(sql)
@@ -39,5 +38,44 @@ print("\n")
 cursor.close()
 conexion.close()
 
+############### Base de datos de la tabla -> categoria ########
+cursor=conexion.cursor()
+sql ='SELECT * FROM categoria'
+cursor.execute(sql)
+registro = cursor.fetchall()
+print(registro)
+print("\n")
+cursor.close()
+conexion.close()
+
+############### Base de datos de la tabla -> ubicacion ########
+cursor=conexion.cursor()
+sql ='SELECT * FROM ubicacion'
+cursor.execute(sql)
+registro = cursor.fetchall()
+print(registro)
+print("\n")
+cursor.close()
+conexion.close()
+
+############### Base de datos de la tabla -> libro ########
+cursor=conexion.cursor()
+sql ='SELECT * FROM libro'
+cursor.execute(sql)
+registro = cursor.fetchall()
+print(registro)
+print("\n")
+cursor.close()
+conexion.close()
+
+############### Base de datos de la tabla -> evaluar ########
+cursor=conexion.cursor()
+sql ='SELECT * FROM evaluar'
+cursor.execute(sql)
+registro = cursor.fetchall()
+print(registro)
+print("\n")
+cursor.close()
+conexion.close()
 
 
