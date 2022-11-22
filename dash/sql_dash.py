afdd47c9a6b273ca9440591ea7538a97cf3381c1
Jegs03 <68675624+Jegs03@users.noreply.github.com>
2022-11-21 23:30:19 -0500

    Update sql_dash.py

--- dash/sql_dash.py
+++ dash/sql_dash.py
@@ -1,5 +1,3 @@
-
-
 def ctAutores():
     return """SELECT ROUND(cal.prom),COUNT (ROUND(cal.prom)) from(
             SELECT autor.nombre,AVG(promedio.avg) as PROM
@@ -7,7 +5,8 @@ def ctAutores():
                     FROM evaluar GRoup by id_libro) AS promedio ON Libro.id=promedio.id_libro
                     GROUP BY autor.nombre
                     ORDER BY PROM DESC) as cal GROUP BY ROUND(cal.prom) ORDER BY ROUND(cal.prom) DESC;"""
- 
+
+
 def ctEditorial():
     return """SELECT ROUND(cal.prom),COUNT (ROUND(cal.prom)) from(
                 SELECT editorial.nombre,AVG(promedio.avg) as PROM
@@ -15,35 +14,40 @@ def ctEditorial():
                         GROUP BY editorial.nombre
                         ORDER BY PROM DESC) as cal GROUP BY ROUND(cal.prom) ORDER BY ROUND(cal.prom) DESC;"""
 
+
 def librosAutor():
     return """SELECT autor.nombre,count(autor.id)
             FROM libro INNER JOIN autor on libro.id_autor=autor.id
+			WHERE autor.nombre != 'N/A'
             GROUP BY autor.id
-            ORDER BY count(autor.id) DESC
-            WHERE autor.nombre != N/A;"""
+            ORDER BY count(autor.id) DESC;"""
+
 
 def librosEditorial():
     return """SELECT editorial.nombre,count(editorial.id)
-                FROM libro INNER JOIN editorial on libro.id_editorial=editorial.id
+                FROM libro inner join editorial on libro.id_editorial=editorial.id
+                WHERE editorial.nombre != 'n/a'
                 GROUP BY editorial.id
-                ORDER BY count(editorial.id) DESC
-                WHERE editorial.nombre != N/A;;"""
+                ORDER BY count(editorial.id) DESC;"""
+
 
 def calificacion_yr():
-    return """SELECT libro.publicacion_yt,AVG(prom.cal)
-            FROM libro INNER JOIN( SELECT id_libro, AVG(calificacion) as cal FROM evaluar GRoup by id_libro) as prom on libro.id=prom.id_libro
-            GROUP BY libro.publicacion_yt
-            ORDER BY AVG(prom.cal) DESC;"""
+    return """SELECT publicacion_yt, Count(publicacion_yt) 
+        from libro 
+        where publicacion_yt!=1 and publicacion_yt!= 2023 AND publicacion_yt!= 2024 AND publicacion_yt!= 2025 AND publicacion_yt!= 2023 AND publicacion_yt!= 2030 AND publicacion_yt>1800
+        group by publicacion_yt order by publicacion_yt;"""
 
-def categoria():
-    return """SELECT categoria.nombre, COUNT (libro.id)
-            from libro INNER JOIN categoria on libro.id_categoria=categoria.id
-            GROUP BY categoria.nombre
-            ORDER BY COUNT (libro.id) DESC
-            WHERE categoria.nombre != N/A;;"""
 
 def prom_aut():
     return """SELECT autor.nombre,AVG(promedio.avg) as PROMEDIO
         FROM (libro inner join autor ON libro.id_autor=autor.id) INNER JOIN (SELECT id_libro, AVG(calificacion) FROM evaluar GRoup by id_libro) AS promedio ON Libro.id=promedio.id_libro
         GROUP BY autor.nombre
         ORDER BY PROMEDIO DESC;"""
+
+
+def categoria():
+    return """SELECT categoria.nombre, COUNT (libro.id)
+            from libro INNER JOIN categoria on libro.id_categoria=categoria.id
+            WHERE categoria.nombre != 'N/A'
+            GROUP BY categoria.nombre
+            ORDER BY COUNT (libro.id) DESC;"""
